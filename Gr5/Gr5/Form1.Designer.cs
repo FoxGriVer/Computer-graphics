1aa30d202ae3cb80a32e9913ce41b532e1af23ee
FoxGriVer <31801168+FoxGriVer@users.noreply.github.com>
2018-06-28 01:30:13 +0300

    Add files via upload

---
+++ Gr5/Gr5/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Gr5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.glControl = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.DrawFigure = new System.Windows.Forms.Button();
+            this.RotateButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // glControl
+            // 
+            this.glControl.AccumBits = ((byte)(0));
+            this.glControl.AutoCheckErrors = false;
+            this.glControl.AutoFinish = false;
+            this.glControl.AutoMakeCurrent = true;
+            this.glControl.AutoSwapBuffers = true;
+            this.glControl.BackColor = System.Drawing.Color.White;
+            this.glControl.ColorBits = ((byte)(32));
+            this.glControl.DepthBits = ((byte)(16));
+            this.glControl.Location = new System.Drawing.Point(349, 12);
+            this.glControl.Name = "glControl";
+            this.glControl.Size = new System.Drawing.Size(1132, 849);
+            this.glControl.StencilBits = ((byte)(0));
+            this.glControl.TabIndex = 0;
+            // 
+            // DrawFigure
+            // 
+            this.DrawFigure.Location = new System.Drawing.Point(12, 12);
+            this.DrawFigure.Name = "DrawFigure";
+            this.DrawFigure.Size = new System.Drawing.Size(331, 77);
+            this.DrawFigure.TabIndex = 1;
+            this.DrawFigure.Text = "Загрузить фигуру ( Q )";
+            this.DrawFigure.UseVisualStyleBackColor = true;
+            this.DrawFigure.Click += new System.EventHandler(this.DrawFigure_Click);
+            // 
+            // RotateButton
+            // 
+            this.RotateButton.Location = new System.Drawing.Point(12, 95);
+            this.RotateButton.Name = "RotateButton";
+            this.RotateButton.Size = new System.Drawing.Size(331, 77);
+            this.RotateButton.TabIndex = 2;
+            this.RotateButton.Text = "Вращать ( R )";
+            this.RotateButton.UseVisualStyleBackColor = true;
+            this.RotateButton.Click += new System.EventHandler(this.RotateButton_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 178);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(331, 77);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Передвигать ( W A S D)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 261);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(331, 77);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Масштабировать ( Z X )";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1493, 873);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.RotateButton);
+            this.Controls.Add(this.DrawFigure);
+            this.Controls.Add(this.glControl);
+            this.Name = "Form1";
+            this.Text = "Lab5";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Tao.Platform.Windows.SimpleOpenGlControl glControl;
+        private System.Windows.Forms.Button DrawFigure;
+        private System.Windows.Forms.Button RotateButton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Gr5/Gr5/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tao.FreeGlut;
+using Tao.OpenGl;
+using Tao.Platform;
+using Tao.DevIl;
+
+namespace Gr5
+{
+    public partial class Form1 : Form
+    {
+        double positionX, positionY;
+        public int imageId;
+        public uint mGlTextureObject;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+            positionX = 0;
+            positionY = 0;
+            imageId = 0;
+            mGlTextureObject = 0;
+            glControl.InitializeContexts();
+            Gl.glClearColor(255, 255, 255, 1);
+            DrawMethod();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Glut.glutInit();
+            Glut.glutInitDisplayMode(Glut.GLUT_RGB | Glut.GLUT_DOUBLE | Glut.GLUT_DEPTH);   // инициализация режима экрана 
+
+            Il.ilInit();        // инициализация библиотеки openIL
+            Il.ilEnable(Il.IL_ORIGIN_SET);
+
+            Gl.glClearColor(255,255,255,1); // установка цвета очистки экрана (RGBA)
+            Gl.glViewport(0, 0, glControl.Width, glControl.Height); // установка порта вывода 
+
+            Gl.glMatrixMode(Gl.GL_PROJECTION);      // активация проекционной матрицы 
+            Gl.glLoadIdentity();                // очистка матрицы
+            Glu.gluOrtho2D(0, 0, glControl.Width, glControl.Height);
+            Gl.glMatrixMode(Gl.GL_MODELVIEW);           // установка объектно-видовой матрицы
+            Gl.glLoadIdentity();
+
+            Gl.glEnable(Gl.GL_DEPTH_TEST);          // начальные настройки OpenGL 
+            Gl.glEnable(Gl.GL_LIGHTING);
+            Gl.glEnable(Gl.GL_LIGHT0);
+        }
+
+        private void DrawFigure_Click(object sender, EventArgs e)
+        {
+            
+        }
+        
+        private void TextureMethod()
+        {
+            // создаем изображение с идентификатором imageId 
+            Il.ilGenImages(1, out imageId);
+            // делаем изображение текущим 
+            Il.ilBindImage(imageId);
+
+            // адрес изображения  
+            string url = @"C:\Users\Pavel\Documents\Visual Studio 2015\Projects\Gr5\Gr5\bin\Debug\space.jpg";
+            if (Il.ilLoadImage(url))
+            {
+
+                // если загрузка прошла успешно 
+                // сохраняем размеры изображения 
+                int width = Il.ilGetInteger(Il.IL_IMAGE_WIDTH);
+                int height = Il.ilGetInteger(Il.IL_IMAGE_HEIGHT);
+
+                // определяем число бит на пиксель 
+                int bitspp = Il.ilGetInteger(Il.IL_IMAGE_BITS_PER_PIXEL);
+
+                switch (bitspp) // в зависимости от полученного результата 
+                {
+                    // создаем текстуру, используя режим GL_RGB или GL_RGBA 
+                    case 24:
+                        mGlTextureObject = MakeGlTexture(Gl.GL_RGB, Il.ilGetData(), width, height);
+                        break;
+                    case 32:
+                        mGlTextureObject = MakeGlTexture(Gl.GL_RGBA, Il.ilGetData(), width, height);
+                        break;
+                }
+                DrawMethod();
+            }
+        }
+
+        private static uint MakeGlTexture(int Format, IntPtr pixels, int w, int h)
+        {
+            // идентификатор текстурного объекта 
+            uint texObject;
+            // генерируем текстурный объект 
+            Gl.glGenTextures(1, out texObject);
+            // устанавливаем режим упаковки пикселей 
+            Gl.glPixelStorei(Gl.GL_UNPACK_ALIGNMENT, 1);
+            // создаем привязку к только что созданной текстуре 
+            Gl.glBindTexture(Gl.GL_TEXTURE_2D, texObject);
+            // устанавливаем режим фильтрации и повторения текстуры 
+            Gl.glTexParameteri(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_WRAP_S, Gl.GL_REPEAT);
+            Gl.glTexParameteri(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_WRAP_T, Gl.GL_REPEAT);
+            Gl.glTexParameteri(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_MAG_FILTER, Gl.GL_LINEAR);
+            Gl.glTexParameteri(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_MIN_FILTER, Gl.GL_LINEAR);
+            Gl.glTexEnvf(Gl.GL_TEXTURE_ENV, Gl.GL_TEXTURE_ENV_MODE, Gl.GL_REPLACE);
+            // создаем RGB или RGBA текстуру 
+            switch (Format)
+            {
+                case Gl.GL_RGB:
+                    Gl.glTexImage2D(Gl.GL_TEXTURE_2D, 0, Gl.GL_RGB, w, h, 0, Gl.GL_RGB, Gl.GL_UNSIGNED_BYTE, pixels);
+                    break;
+
+                case Gl.GL_RGBA:
+                    Gl.glTexImage2D(Gl.GL_TEXTURE_2D, 0, Gl.GL_RGBA, w, h, 0, Gl.GL_RGBA, Gl.GL_UNSIGNED_BYTE, pixels);
+                    break;
+            }
+            // возвращаем идентификатор текстурного объекта 
+            return texObject;
+        }
+
+        public void DrawMethod()
+        {            
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
+            Gl.glClearColor(255, 255, 255, 1);
+
+            Gl.glEnable(Gl.GL_TEXTURE_2D);
+            Gl.glBindTexture(Gl.GL_TEXTURE_2D, mGlTextureObject);               // включаем режим текстурирования, указывая идентификатор mGlTextureObject 
+
+            Gl.glBegin(Gl.GL_POLYGON);
+            Gl.glVertex2d(0, 0.75);
+            Gl.glTexCoord2f(0, (float)0.75);
+            Gl.glVertex2d(0.65, 0.25);
+            Gl.glTexCoord2f((float)0.65, (float)0.25);
+            Gl.glVertex2d(0.4, -0.6);
+            Gl.glTexCoord2f((float)0.4, (float)-0.6);
+            Gl.glVertex2d(-0.4, -0.6);
+            Gl.glTexCoord2f((float)-0.4, (float)-0.6);
+            Gl.glVertex2d(-0.65, 0.25);
+            Gl.glTexCoord2f((float)-0.65, (float)0.25);
+            Gl.glEnd();
+
+
+            //Gl.glBegin(Gl.GL_LINE_STRIP);
+
+            //Gl.glVertex2d(0, 0.75);
+            //Gl.glTexCoord2f(0, (float)0.75);
+
+            //Gl.glVertex2d(0.15, 0.25);
+            //Gl.glTexCoord2f((float)0.15, (float)0.25);
+
+            //Gl.glVertex2d(0.65, 0.25);
+            //Gl.glTexCoord2f((float)0.65, (float)0.25);
+
+            //Gl.glVertex2d(0.25, -0.13);
+            //Gl.glTexCoord2f((float)0.25, (float)-0.13);
+
+            //Gl.glVertex2d(0.4, -0.6);
+            //Gl.glTexCoord2f((float)0.4, (float)-0.6);
+
+            //Gl.glVertex2d(0, -0.3);
+            //Gl.glTexCoord2f(0, (float)-0.3);
+
+            //Gl.glVertex2d(-0.4, -0.6);
+            //Gl.glTexCoord2f((float)-0.4, (float)-0.6);
+
+            //Gl.glVertex2d(-0.25, -0.13);
+            //Gl.glTexCoord2f((float)-0.25, (float)-0.13);
+
+            //Gl.glVertex2d(-0.65, 0.25);
+            //Gl.glTexCoord2f((float)-0.65, (float)0.25);
+
+            //Gl.glVertex2d(-0.15, 0.25);
+            //Gl.glTexCoord2f((float)-0.15, (float)0.25);
+
+            //Gl.glVertex2d(0, 0.75);
+            //Gl.glTexCoord2f(0, (float)0.75);
+
+            //Gl.glEnd();
+
+            Gl.glFlush();
+            Gl.glDisable(Gl.GL_TEXTURE_2D);                 // отключаем режим текстурирования 
+            glControl.Invalidate();
+        }
+
+        private void RotateButton_Click(object sender, EventArgs e)
+        {
+            Gl.glRotated((double)15, 0, 0, 1);
+            DrawMethod();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyData == Keys.D)
+            {
+                positionX += 0.1;
+                Gl.glTranslated(positionX, 0, 0);
+                positionX -= 0.1;
+            }
+            if (e.KeyData == Keys.A)
+            {
+                positionX += 0.1;
+                Gl.glTranslated(-positionX, 0, 0);
+                positionX -= 0.1;
+            }
+            if (e.KeyData == Keys.W)
+            {
+                positionY += 0.1;
+                Gl.glTranslated(0, positionY, 0);
+                positionY -= 0.1;
+            }
+            if (e.KeyData == Keys.S)
+            {
+                positionY += 0.1;
+                Gl.glTranslated(0, -positionY, 0);
+                positionY -= 0.1;
+            }
+            if (e.KeyData == Keys.R)
+            {
+                Gl.glRotated((double)15, 0, 0, 1);
+            }
+            if (e.KeyData == Keys.Z)
+            {
+                Gl.glScaled(1.1, 1.1, 1);
+            }
+            if (e.KeyData == Keys.X)
+            {
+                Gl.glScaled(0.9, 0.9, 1);
+            }
+            //DrawMethod();
+            TextureMethod();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            positionX += 0.1;
+            Gl.glTranslated(positionX, 0, 0);
+            positionX -= 0.1;
+            DrawMethod();
+        }        
+    }
+}
