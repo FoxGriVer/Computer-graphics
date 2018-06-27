8be533e1ae8680f983653b3ff9b1d0e857bd0cee
FoxGriVer <31801168+FoxGriVer@users.noreply.github.com>
2018-06-28 01:24:43 +0300

    Add files via upload

---
+++ GR2/GR2/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace GR2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox1.Location = new System.Drawing.Point(6, 109);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1283, 1227);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(981, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(308, 91);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Draw Carpet";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(271, 43);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(57, 33);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(334, 43);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(57, 33);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(397, 43);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(57, 33);
+            this.radioButton3.TabIndex = 4;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(460, 43);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(57, 33);
+            this.radioButton4.TabIndex = 5;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "4";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(523, 43);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(57, 33);
+            this.radioButton5.TabIndex = 6;
+            this.radioButton5.Text = "5";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(238, 29);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Выберите порядок:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1301, 823);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ GR2/GR2/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GR2
+{
+    public partial class Form1 : Form
+    {
+        private int order0 = 0;
+        private int order;
+        private int width;
+        private int height;
+        private Bitmap fractal;
+        private Graphics gr;
+        RectangleF carpet;
+
+
+        public Form1()
+        {            
+            InitializeComponent();
+            width = pictureBox1.Width;
+            height = pictureBox1.Height;
+            fractal = new Bitmap(width, height);
+            gr = Graphics.FromImage(fractal);
+            carpet = new RectangleF(0, 0, width, height);
+        }
+
+        public int GetOrder()
+        {
+            if (radioButton1.Checked)
+            {
+                order = 1;
+            }
+            if (radioButton2.Checked)
+            {
+                order = 2;
+            }
+            if (radioButton3.Checked)
+            {
+                order = 3;
+            }
+            if (radioButton4.Checked)
+            {
+                order = 4;
+            }
+            if (radioButton5.Checked)
+            {
+                order = 5;
+            }
+            return order;
+        }
+
+        public void Draw5Carpet()
+        {
+            System.Threading.Thread.Sleep(1000);
+            if (pictureBox1.BackgroundImage != null)
+            {
+                gr.Clear(Color.White);
+            }
+            order0++;
+            DrawCarpet(order0, carpet);
+            pictureBox1.BackgroundImage = fractal;
+            pictureBox1.Refresh();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            order = GetOrder();
+            while (order0 != order)
+            {
+                Draw5Carpet();
+            }
+            order0 = 0;
+        }
+
+        public void DrawCarpet(int order, RectangleF carpet)
+        {
+            if (order == 0)
+            {
+                gr.FillRectangle(Brushes.Black, carpet);
+            }
+            else
+            {
+                var width = carpet.Width / 3f;
+                var height = carpet.Height / 3f;
+
+                var x1 = carpet.Left;
+                var x2 = x1 + width;
+                var x3 = x1 + 2f * width;
+
+                var y1 = carpet.Top;
+                var y2 = y1 + height;
+                var y3 = y1 + 2f * height;
+
+                DrawCarpet(order - 1, new RectangleF(x1, y1, width, height));
+                DrawCarpet(order - 1, new RectangleF(x2, y1, width, height));
+                DrawCarpet(order - 1, new RectangleF(x3, y1, width, height));
+                DrawCarpet(order - 1, new RectangleF(x1, y2, width, height)); 
+                DrawCarpet(order - 1, new RectangleF(x3, y2, width, height)); 
+                DrawCarpet(order - 1, new RectangleF(x1, y3, width, height));
+                DrawCarpet(order - 1, new RectangleF(x2, y3, width, height));
+                DrawCarpet(order - 1, new RectangleF(x3, y3, width, height));
+            }
+        }
+    }
+}
